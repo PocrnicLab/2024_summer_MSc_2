797775a822244475a01e4b7a2c8f12bdf7e5ac6a
Cheng Zeng <s2478435@node1h21.ecdf.ed.ac.uk>
2024-07-01 11:19:23 +0100

    Remove snp with 0 position

--- 01_Data/scripts/step1.update_snp_map_to_3.1.py
+++ 01_Data/scripts/step1.update_snp_map_to_3.1.py
@@ -17,6 +17,9 @@ def update_positions(file1_path, file2_path, output_path):
     # Select necessary columns, keeping only rows present in file1
     result_df = merged_df[file2.columns]
     
+    # Filter out rows where Chromosome or Position is 0
+    result_df = result_df[(result_df['Chromosome'] != 0) & (result_df['Position'] != 0)]
+    
     # Sort the result by Index
     result_df = result_df.sort_values(by='Index')
     
